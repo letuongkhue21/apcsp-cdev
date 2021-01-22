5f971b9d7642b3db722ab8b2d3e8c64b55bc034f
Khue <letuongkhue@gmail.com>
2021-01-22 06:37:49 +0000

    Project uploaded

--- proj/encrypt/encrypt.c
+++ proj/encrypt/encrypt.c
@@ -10,9 +10,17 @@
 char CHARS[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 int CHARS_LEN = 62;
 
-
+int find_index(char c)
+ {
+  for (int i = 0; i < 62; i++)
+   {
+     if (CHARS[i] == c)
+     {
+       return i;
+     }
+   }
+} 
 char shiftChar(char c, int shift, int direction)
-{
   // implement the character shift here:
   //  given a char c shift it either forwards (direction == 1) or backwards (direction == 0)
   //  use the CHARS array above
@@ -21,9 +29,30 @@ char shiftChar(char c, int shift, int direction)
   // eg.
   //   shiftChar('c', 3, 1) : 'f'
   //   shiftChar('S', 2, 0) : 'P'
-  //   shiftChar('b', 3, 0) : '8'
+  //   shiftChar('b', 3, 0) : '
+
+{
+  int realshift;
+  int index;
+  
+  if (c== ' ')
+  {
+   return ' ';
+  }
+ 
+  if ( direction == 1)
+  {
+    realshift = shift;
+  }
+  else
+  {
+   realshift = 62 - shift;
+  }
+  index = CHARS[(find_index(c) + realshift) % 62];
+  return index;
 }
 
+ 
 
 void encrypt(char str[], int shifts[], int shiftslen)
 {
